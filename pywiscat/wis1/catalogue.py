f9a1cfdc1fe9314e604a002cbe22c4616f162a40
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-14 22:15:17 +0000

    Adding tarfile member sanitization to extractall()

--- pywiscat/wis1/catalogue.py
+++ pywiscat/wis1/catalogue.py
@@ -59,7 +59,26 @@ def cache_catalogue(directory: str) -> bool:
     os.makedirs(directory, exist_ok=True)
     content = io.BytesIO(urlopen(zipfile_url).read())
     with tarfile.open(fileobj=content, mode='r:gz') as z:
-        z.extractall(directory)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(z, directory)
 
     return True
 
